66476565a68d693a8b92becb99684c4266b7375e
Telemachos Chatzithedorou <30343962+telemachosc@users.noreply.github.com>
2019-07-08 18:04:43 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,7 @@
 // Task Timer, Developed by Telemachos Chatzitheodorou ////////////////////////////////
 // This is a simple task timer app that let's you determine the time you need/////////
 // to complete certain tasks using the pomodoro technique. You can learn more////////
-// about the pomodoro technique on https://en.wikipedia.org/wiki/Pomodoro_Technique ///
+// about the pomodoro technique on https://en.wikipedia.org/wiki/Pomodoro_Technique 
 // Feel free to use it as you like.////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
 
